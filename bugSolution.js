8ddcd1115a5ac31a59db7793600e4a8e6f8c673b
BugHunter-X <askzenai@gmail.com>
2024-12-04 18:53:54 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,20 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* Solution: Moved wildcard route to the end */}
+        <Route path="/contact/*" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {  return <div>Home</div>;}
+function About() { return <div>About</div>;}
+function Contact() { return <div>Contact</div>;}
+```
